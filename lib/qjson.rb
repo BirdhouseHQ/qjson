ba2ef1d679122b03c3820c753c2c12d90e83f6a9
Daniel Staudigel <dstaudigel@gmail.com>
2016-03-09 12:30:16 -0800

    Reformat for brevity.

--- lib/qjson.rb
+++ lib/qjson.rb
@@ -3,20 +3,22 @@ require 'qjson/collection_serializer'
 require 'qjson/base'
 
 module QJSON
-  # the two main entry points to QJSON, `render`, and `parse`
-  def self.render(record,context,version,options={})
-    serializer = SerializerCache.find(record,context,version)
-    serializer.render(record,version,options)
-  end
+  class << self 
+    # the two main entry points to QJSON, `render`, and `parse`
+    def render(record,context,version,options={})
+      serializer = SerializerCache.find(record,context,version)
+      serializer.render(record,version,options)
+    end
 
-  def self.render_collection(collection,context,version,options={})
-    collection_serializer = CollectionSerializer.new
+    def render_collection(collection,context,version,options={})
+      collection_serializer = CollectionSerializer.new
 
-    collection_serializer.render(collection,context,version,options)
-  end
+      collection_serializer.render(collection,context,version,options)
+    end
 
-  def self.parse(record,json,context,version)
-    serializer = SerializerCache.find(record,context,version)
-    serializer.parse(record,json,version)
+    def parse(record,json,context,version)
+      serializer = SerializerCache.find(record,context,version)
+      serializer.parse(record,json,version)
+    end
   end
 end
