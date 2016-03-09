1166672d1cfd617efefd3abb9bce25e8673d73f7
Daniel Staudigel <dstaudigel@gmail.com>
2016-03-09 12:30:37 -0800

    Reformat for more normal module declaration style.

--- lib/qjson/serializer_cache.rb
+++ lib/qjson/serializer_cache.rb
@@ -1,69 +1,71 @@
-class QJSON::SerializerCache
-  # View RE: app/views/api/<possibly namespaced model name>/<context>.<version>.<qjrb or rb>
+module QJSON
+  class SerializerCache
+    # View RE: app/views/api/<possibly namespaced model name>/<context>.<version>.<qjrb or rb>
 
 
-  def self.cache(name,context,version,hash=nil)
-    @_cache ||= {}
-    @_cache[name] ||= []
+    def self.cache(name,context,version,hash=nil)
+      @_cache ||= {}
+      @_cache[name] ||= []
 
-  end
+    end
 
-  def self.path_for(name,context,version)
-    version = version.to_s
+    def self.path_for(name,context,version)
+      version = version.to_s
 
-    all_options = Dir.glob("#{Rails.root}/app/views/api/#{name}/#{context.to_s}.*")
+      all_options = Dir.glob("#{Rails.root}/app/views/api/#{name}/#{context.to_s}.*")
 
-    file_path = nil
-    file_version = nil
+      file_path = nil
+      file_version = nil
 
-    all_options.each do |path|
-      m = path.match(QJSON::Base::VIEW_RE)
+      all_options.each do |path|
+        m = path.match(QJSON::Base::VIEW_RE)
 
-      # extension not qjrb or rb is most common reason to skip
-      next unless m
+        # extension not qjrb or rb is most common reason to skip
+        next unless m
 
-      f_model_name = m[1]
-      f_context = m[2]
-      f_version = m[3]
-      f_ext = m[4]
+        f_model_name = m[1]
+        f_context = m[2]
+        f_version = m[3]
+        f_ext = m[4]
 
-      next unless f_context == context.to_s
-      next unless f_model_name == name
+        next unless f_context == context.to_s
+        next unless f_model_name == name
 
-      if(!file_version || ( file_version < f_version && f_version <= version ) )
-        file_path = path
-        file_version = f_version
+        if(!file_version || ( file_version < f_version && f_version <= version ) )
+          file_path = path
+          file_version = f_version
+        end
+      end
+
+      file_path
+    end
+
+    def self.load_path(path)
+      return Class.new(QJSON::Base) unless path
+
+      if(path.match(/\.rb$/))
+        c = Class.new(QJSON::Base)
+        c.source_path(path)
+        c.class_eval File.read(path)
+        c
+      elsif(path.match(/\.qjrb$/))
+        c = Class.new(QJSON::Base)
+        c.source_path(path)
+        c.class_eval "def operate\n"+File.read(path)+"\nend"
+        c
+      else
+        raise "Invalid Extension #{File.extname(path)}"
       end
     end
 
-    file_path
-  end
-
-  def self.load_path(path)
-    return Class.new(QJSON::Base) unless path
-
-    if(path.match(/\.rb$/))
-      c = Class.new(QJSON::Base)
-      c.source_path(path)
-      c.class_eval File.read(path)
-      c
-    elsif(path.match(/\.qjrb$/))
-      c = Class.new(QJSON::Base)
-      c.source_path(path)
-      c.class_eval "def operate\n"+File.read(path)+"\nend"
-      c
-    else
-      raise "Invalid Extension #{File.extname(path)}"
+    def self.class_for(name,context,version)
+      path = path_for(name,context,version)
+      puts "Could not find path for #{name} #{context} #{version}" unless path
+      load_path(path)
     end
-  end
 
-  def self.class_for(name,context,version)
-    path = path_for(name,context,version)
-    puts "Could not find path for #{name} #{context} #{version}" unless path
-    load_path(path)
-  end
-
-  def self.find(record,context,version)
-    class_for(record.class.name.underscore.pluralize,context,version)
+    def self.find(record,context,version)
+      class_for(record.class.name.underscore.pluralize,context,version)
+    end
   end
 end
